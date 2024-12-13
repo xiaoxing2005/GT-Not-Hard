25239e9171587336b33b6a8623b7984d1aa56d09
z1564058782 <40727986+z1564058782@users.noreply.github.com>
2024-12-13 09:54:47 +0800

    test

--- src/main/java/loader/MachinesLoader.java
+++ src/main/java/loader/MachinesLoader.java
@@ -1,8 +1,11 @@
 package loader;
 
+import machines.Chaos;
+
 public class MachinesLoader {
     public static void loaderMachines(){
         //LargeChemicalReactorTest largeChemicalReactorTest = new LargeChemicalReactorTest(20001,"LargeChemicalReactorTest","aNameLargeChemicalReactorTest");
+        Chaos Chaos = new Chaos(20001, "Chaos", "Chaos");
     }
 
 }
