1a779f2f854f507bd7b41f90404c93b63677a493
z1564058782 <40727986+z1564058782@users.noreply.github.com>
2025-01-03 21:26:12 +0800

    test

--- src/main/java/com/mofoga/gtnothard/CommonProxy.java
+++ src/main/java/com/mofoga/gtnothard/CommonProxy.java
@@ -31,7 +31,6 @@ public class CommonProxy {
     // postInit "Handle interaction with other mods, complete your setup based on this." (Remove if not needed)
     public void postInit(FMLPostInitializationEvent event) {}
 
-
     // register server commands in this event handler (Remove if not needed)
     public void serverStarting(FMLServerStartingEvent event) {}
 }
